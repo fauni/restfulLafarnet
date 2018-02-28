29d11c61f2ae96bb46642560a3a5ae6920c18385
FRANZ-PC\Franz Aruni <amrfranz@gmail.com>
2018-02-28 14:58:18 -0400

    PUBLICADO PRIMERA FASE

--- app/lib/database.php
+++ app/lib/database.php
@@ -13,6 +13,11 @@ class Database
         $DB_pass = "";
         $DB_name = "newlafarnet";
 
+        /*$DB_host = "192.168.1.213";
+        $DB_user = "lafaradmDB";
+        $DB_pass = "";
+        $DB_name = "newlafarnet";*/
+
         $pdo = new PDO('mysql:host='.$DB_host.';dbname='.$DB_name.';charset=utf8', $DB_user, $DB_pass);
         
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
