fe85f2e167a114ce7f598a1b6956de0e716adbbf
severo cruz <severo_86@hotmail.com>
2018-04-18 12:10:30 -0400

    CARACTERISTICAS PARA  PARAMETROS

---
+++ app/model/sacc_caracteristicas_model.php
@@ -0,0 +1,72 @@
+<?php
+namespace App\Model;
+
+//use App\Entitie\Area;
+use App\Lib\Database;
+//use App\Lib\Hash;
+use App\Http\Response;
+
+class CaracteristicasModel
+{
+    private $db;
+    private $table = 'sacc_clasificacion_caracteristicas';
+    private $response;
+    
+    public function __CONSTRUCT()
+    {
+        $this->db = Database::StartUp();
+        $this->response = new Response();
+    }
+    
+   public function GetAllMP()
+    {
+        try
+        {
+            $result = array();
+            $stm = $this->db->prepare("select  * from sacc_productos_caracteristicas_fisicas 
+                                        where tipo = 1
+                                        union
+                                        select * from sacc_productos_analisis_quimico
+                                        where tipo = 1
+                                        union
+                                        select * from sacc_productos_analisis_microbiologico where tipo = 1;");
+            $stm->execute();
+            
+            $this->response->setStatus(200);
+            $this->response->setBody($stm->fetchAll());
+            $this->response->message=$this->response->getMessageForCode(200);
+            return $this->response;
+        }
+        catch(Exception $e)
+        {
+            $this->response->setStatus($e->getCode());
+            return $this->response;
+        }
+    }
+     public function GetAllPT()
+    {
+        try
+        {
+            $result = array();
+            $stm = $this->db->prepare("select  * from sacc_productos_caracteristicas_fisicas 
+                                        where tipo = 2
+                                        union
+                                        select * from sacc_productos_analisis_quimico
+                                        where tipo = 2
+                                        union
+                                        select * from sacc_productos_analisis_microbiologico where tipo = 2;");
+            $stm->execute();
+            
+            $this->response->setStatus(200);
+            $this->response->setBody($stm->fetchAll());
+            $this->response->message=$this->response->getMessageForCode(200);
+            return $this->response;
+        }
+        catch(Exception $e)
+        {
+            $this->response->setStatus($e->getCode());
+            return $this->response;
+        }
+    }
+
+}
---
+++ app/route/sacc_caracteristicas_route.php
@@ -0,0 +1,38 @@
+<?php
+use App\Model\CaracteristicasModel;
+//use Slim\Http\UploadedFile;
+//use Slim\Http\Request;
+//use Slim\Http\Response;
+
+$app->group('/caracteristicas/', function () {
+    
+    $this->get('test', function ($req, $res, $args) {
+        return $res->getBody()
+                   ->write('Hola Users');
+    });
+    
+    $this->get('getMP', function ($req, $res, $args) {
+        $um = new CaracteristicasModel();
+        $this->logger->info("Consulta Analistas: ".$res); 
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                $um->GetAllMP()
+            )
+        );
+    }); 
+    $this->get('getPT', function ($req, $res, $args) {
+        $um = new CaracteristicasModel();
+        $this->logger->info("Consulta Analistas: ".$res); 
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                $um->GetAllPT()
+            )
+        );
+    });  
+});
